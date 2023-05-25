57b1ce5c05eb8a980cf056fc98156d0790a93a61
YasminaMedjri <98921744+Yasmina3@users.noreply.github.com>
2023-05-25 19:26:13 +0100

    Update CSP design

--- CSPLogic.py
+++ CSPLogic.py
@@ -7,7 +7,7 @@ class Application(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Country Data Entry")
-        self.configure(bg='#95F8E4')  # Set background color
+        self.configure(bg='#0E153A')  # Set background color
         self.country_data = []
 
         self.geometry("900x600")  # Set window size
@@ -31,13 +31,13 @@ class Application(tk.Tk):
         self.num_countries_entry.config(state=tk.DISABLED)
         self.num_countries_button.config(state=tk.DISABLED)
 
-        self.subtitle_label = tk.Label(self, text="Enter neighboring countries separated by commas i.e 1,2,3", font=("Helvetica", 14), bg='#95F8E4')
+        self.subtitle_label = tk.Label(self, text="Enter neighboring countries separated by commas i.e 1,2,3", font=("Helvetica", 14))
         self.subtitle_label.pack(pady=10)
 
         self.scrollable_frame = ttk.Frame(self, height=200)  
         self.scrollable_frame.pack(fill=tk.BOTH, expand=True)
 
-        self.canvas = tk.Canvas(self.scrollable_frame, bg='#95F8E4', height=200)  
+        self.canvas = tk.Canvas(self.scrollable_frame, bg='#0E153A', height=200)  
         self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
         self.scrollbar = ttk.Scrollbar(self.scrollable_frame, orient=tk.VERTICAL, command=self.canvas.yview)
@@ -46,12 +46,12 @@ class Application(tk.Tk):
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
         self.canvas.bind("<Configure>", self.adjust_canvas_scrollregion)
 
-        self.inner_frame = tk.Frame(self.canvas, bg='#95F8E4')
+        self.inner_frame = tk.Frame(self.canvas, bg='#0E153A')
         self.canvas.create_window((0, 0), window=self.inner_frame, anchor=tk.NW)
 
         self.country_entries = []
         for i in range(num_countries):
-            label = tk.Label(self.inner_frame, text=f"Country {i}:", font=("Helvetica", 12, "bold"), bg='#95F8E4')
+            label = tk.Label(self.inner_frame, text=f"Country {i}:", font=("Helvetica", 12, "bold"), bg='#0E153A', fg='#28c7a6')
             label.pack()
             entry = tk.Entry(self.inner_frame, font=("Helvetica", 12))
             entry.pack()
