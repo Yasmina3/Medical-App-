13f78c2caae16fe2a0d1a4c0525b99f7e4031d17
Nour  El Yakine Guendouz <120640762+YAKINE19@users.noreply.github.com>
2024-04-03 14:14:52 +0200

    Delete informed_uninformedLogic.py

--- informed_uninformedLogic.py
+++
@@ -1,6 +0,0 @@
-def run_logic(window, pageTitle):
-    import graph_gui
-    
-    window.geometry("{}x{}+0+0".format(window.winfo_screenwidth(), window.winfo_screenheight()))
-    app = graph_gui.Application(master=window)
-    app.mainloop()
