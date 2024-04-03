04d5ab454d1de710eede93d58579f7b22ab2059d
Nour  El Yakine Guendouz <120640762+YAKINE19@users.noreply.github.com>
2024-04-03 14:10:04 +0200

    Delete Desktop/3rdYearAI/S2/Group Project/Medical_App directory

--- Desktop/3rdYearAI/S2/Group Project/Medical_App/lib/presentation/doctor_detail_screen/models/time_item_model.dart
+++
@@ -1,8 +0,0 @@
-import '../../../core/app_export.dart';
-
-/// This class is used in the [time_item_widget] screen.
-class TimeItemModel {
-  Rx<String>? am = Rx("09:00 AM");
-
-  Rx<bool>? isSelected = Rx(false);
-}
