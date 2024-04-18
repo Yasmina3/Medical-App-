5d17d292aee1f096d9017890e9139e8375bedd5f
YasminaMedjri <98921744+Yasmina3@users.noreply.github.com>
2024-04-18 07:04:18 +0100

    Search final

--- tabibak/settings.py
+++ tabibak/settings.py
@@ -30,9 +30,8 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-ALLOWED_HOSTS = ['127.0.0.1','10.0.2.2','localhost']
-
+# or, to allow all domains (for testing only):
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
