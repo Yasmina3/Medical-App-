24c21e6080b6084c51cb1425e424450f3e2c065f
FaridaBoubekeur <120862244+FaridaBoubekeur@users.noreply.github.com>
2024-04-18 05:28:32 +0100

    Update api_urls.dart

--- lib/core/utils/api_urls.dart
+++ lib/core/utils/api_urls.dart
@@ -5,5 +5,6 @@ class Endpoints{
   static const String get_all_doctors = "http://${Constants.djangoHost}/Endpoints/All_doctors/";
   static const String get_all_specialties = "http://${Constants.djangoHost}/Endpoints/All_specialties/";
   static const String add_patient = "http://${Constants.djangoHost}/Endpoints/register_user/";
+  static const String edit_profile = "http://${Constants.djangoHost}/Endpoints/edit_profile";
 
 }
