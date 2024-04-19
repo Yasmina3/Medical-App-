1e7a9d00bc0736f05f81a967d0111ca4a36699ef
FaridaBoubekeur <120862244+FaridaBoubekeur@users.noreply.github.com>
2024-04-19 18:51:39 +0100

    Update api.dart

--- lib/presentation/edit_profile_screen/api.dart
+++ lib/presentation/edit_profile_screen/api.dart
@@ -9,31 +9,19 @@ Future<String?> api_edit_profile({
   required String email,
 }) async {
   try {
-    final response = await http.post(
-      Uri.parse('${Endpoints.edit_profile}/$patient_id/'),
-      body: jsonEncode({
-        "full_name": fullName,
-        "password": password,
-        "email": email,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    );
+    final response = await http
+        .post(Uri.parse('${Endpoints.edit_profile}/$patient_id/'), body: {
+      "full_name": fullName,
+      "password": password,
+      "email": email,
+    });
+    print(fullName);
 
     if (response.statusCode == 200) {
-      // If the status code is 200, the profile update was successful
-      return null; // No error message
-    } else if (response.statusCode == 403) {
-      // If the status code is 403, indicating a forbidden error
-      return 'Incorrect password'; // Return specific error message
-    } else {
-      // If any other status code is received, return a generic error message
-      return 'An error occurred';
+      return jsonDecode(response.body);
     }
   } catch (error) {
-    // If an error occurs during the API call, return a generic error message
-    print("Error: $error");
-    return 'An error occurred';
+    print("Error yes ${error.toString()}");
+    return null;
   }
 }
