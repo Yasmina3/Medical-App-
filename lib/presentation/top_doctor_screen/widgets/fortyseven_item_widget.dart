5cc82cb0240b235ed84189546df86f110ba762dd
FaridaBoubekeur <120862244+FaridaBoubekeur@users.noreply.github.com>
2024-04-23 20:07:29 +0100

    Update fortyseven_item_widget.dart

--- lib/presentation/top_doctor_screen/widgets/fortyseven_item_widget.dart
+++ lib/presentation/top_doctor_screen/widgets/fortyseven_item_widget.dart
@@ -1,287 +1,87 @@
 import '../models/fortyseven_item_model.dart';
-import '../controller/top_doctor_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:tabibak/core/app_export.dart';
+import 'package:flutter/material.dart';
 
-// ignore: must_be_immutable
 class FortysevenItemWidget extends StatelessWidget {
-  FortysevenItemWidget(
-    this.fortysevenItemModelObj, {
-    Key? key,
-  }) : super(
-          key: key,
-        );
+  final Map<dynamic, dynamic> doctorData;
 
-  FortysevenItemModel fortysevenItemModelObj;
-
-  var controller = Get.find<TopDoctorController>();
+  FortysevenItemWidget({required this.doctorData});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        SizedBox(
-          height: 125.v,
-          width: 239.h,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: SizedBox(
-                  height: 125.v,
-                  width: 217.h,
-                  child: Stack(
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                          margin: EdgeInsets.only(right: 10.h),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 50.h,
-                            vertical: 34.v,
-                          ),
-                          decoration: AppDecoration.outlineBlueGray.copyWith(
-                            borderRadius: BorderRadiusStyle.roundedBorder10,
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 13.v),
-                              Obx(
-                                () => Text(
-                                  fortysevenItemModelObj.chardiologist!.value,
-                                  style: theme.textTheme.labelLarge,
-                                ),
-                              ),
-                              SizedBox(height: 6.v),
-                              SizedBox(
-                                height: 18.v,
-                                width: 41.h,
-                                child: Stack(
-                                  alignment: Alignment.bottomRight,
-                                  children: [
-                                    Align(
-                                      alignment: Alignment.center,
-                                      child: Container(
-                                        height: 18.v,
-                                        width: 41.h,
-                                        padding: EdgeInsets.symmetric(
-                                          horizontal: 3.h,
-                                          vertical: 2.v,
-                                        ),
-                                        decoration:
-                                            AppDecoration.fillBlueGray.copyWith(
-                                          borderRadius:
-                                              BorderRadiusStyle.roundedBorder3,
-                                        ),
-                                        child: Obx(
-                                          () => CustomImageView(
-                                            imagePath: fortysevenItemModelObj
-                                                .chardiologist1!.value,
-                                            height: 13.adaptSize,
-                                            width: 13.adaptSize,
-                                            alignment: Alignment.bottomLeft,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    Align(
-                                      alignment: Alignment.bottomRight,
-                                      child: Padding(
-                                        padding: EdgeInsets.only(right: 3.h),
-                                        child: Obx(
-                                          () => Text(
-                                            fortysevenItemModelObj
-                                                .fortySeven!.value,
-                                            style: CustomTextStyles
-                                                .labelLargePrimary,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: EdgeInsets.only(right: 37.h),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Obx(
-                                () => CustomImageView(
-                                  imagePath:
-                                      fortysevenItemModelObj.mdechezvous!.value,
-                                  height: 13.adaptSize,
-                                  width: 13.adaptSize,
-                                  margin: EdgeInsets.only(bottom: 1.v),
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(left: 3.h),
-                                child: Obx(
-                                  () => Text(
-                                    fortysevenItemModelObj.distance!.value,
-                                    style: theme.textTheme.labelLarge,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 18.v),
-                          child: Obx(
-                            () => Text(
-                              fortysevenItemModelObj.drMarcusHorizon!.value,
-                              style: theme.textTheme.titleMedium,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 21.h),
-                  decoration: AppDecoration.outlineBlueGray.copyWith(
-                    borderRadius: BorderRadiusStyle.roundedBorder10,
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 18.v),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Obx(
-                          () => Text(
-                            fortysevenItemModelObj.drMarcusHorizon1!.value,
-                            style: theme.textTheme.titleMedium,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 11.v),
-                      Padding(
-                        padding: EdgeInsets.only(left: 29.h),
-                        child: Obx(
-                          () => Text(
-                            fortysevenItemModelObj.widget!.value,
-                            style: theme.textTheme.titleSmall,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 2.v),
-                      Container(
-                        height: 18.v,
-                        width: 41.h,
-                        margin: EdgeInsets.only(left: 29.h),
-                        child: Stack(
-                          alignment: Alignment.bottomRight,
-                          children: [
-                            Align(
-                              alignment: Alignment.center,
-                              child: Container(
-                                height: 18.v,
-                                width: 41.h,
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: 3.h,
-                                  vertical: 2.v,
-                                ),
-                                decoration: AppDecoration.fillBlueGray.copyWith(
-                                  borderRadius:
-                                      BorderRadiusStyle.roundedBorder3,
-                                ),
-                                child: Obx(
-                                  () => CustomImageView(
-                                    imagePath: fortysevenItemModelObj
-                                        .chardiologist2!.value,
-                                    height: 13.adaptSize,
-                                    width: 13.adaptSize,
-                                    alignment: Alignment.bottomLeft,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment.bottomRight,
-                              child: Padding(
-                                padding: EdgeInsets.only(right: 3.h),
-                                child: Obx(
-                                  () => Text(
-                                    fortysevenItemModelObj.fortySeven1!.value,
-                                    style: CustomTextStyles.labelLargePrimary,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 18.v),
-                      Padding(
-                        padding: EdgeInsets.only(left: 29.h),
-                        child: Row(
-                          children: [
-                            Obx(
-                              () => CustomImageView(
-                                imagePath:
-                                    fortysevenItemModelObj.widget1!.value,
-                                height: 13.adaptSize,
-                                width: 13.adaptSize,
-                                margin: EdgeInsets.only(bottom: 3.v),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(left: 3.h),
-                              child: Obx(
-                                () => Text(
-                                  fortysevenItemModelObj.eightHundred!.value,
-                                  style: theme.textTheme.labelLarge,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
           ),
-        ),
-        Obx(
-          () => CustomImageView(
-            imagePath: fortysevenItemModelObj.chardiologist3!.value,
-            height: 111.adaptSize,
-            width: 111.adaptSize,
-            radius: BorderRadius.circular(
-              8.h,
-            ),
-            margin: EdgeInsets.only(
-              top: 8.v,
-              bottom: 6.v,
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.asset(
+              doctorData['gender'] == 'F'
+                  ? "assets/images/DocF.png" // Path for female
+                  : "assets/images/DocM.png", // Path for male
+              width: 100.adaptSize,
+              height: 100.adaptSize,
             ),
           ),
-        ),
-      ],
+          SizedBox(width: 16.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  doctorData['full_name'],
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  doctorData['speciality_name'],
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CustomImageView(
+                      imagePath: "assets/images/img_linkedin.svg",
+                      height: 15.adaptSize,
+                      width: 15.adaptSize,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 5.h, top: 3.v),
+                      child: Text(
+                        doctorData['location'] ?? "",
+                        style: CustomTextStyles.interGray500
+                            .copyWith(fontSize: 15.0),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
